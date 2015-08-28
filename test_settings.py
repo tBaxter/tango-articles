a9e1ff64eac3f8ce2ced40cf05d2beaf90549221
Tim Baxter <tim.baxter@cerner.com>
2015-08-28 10:58:17 -0500

    added travis.yml and test_settings

---
+++ test_settings.py
@@ -0,0 +1,30 @@
+SECRET_KEY = "lorem ipsum"
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.humanize',
+    'django.contrib.sites',
+
+    'tango_shared',
+    'articles',
+    'typogrify' # installed by shared, keeps templates happy
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ROOT_URLCONF = 'articles.urls'
+SITE_ID = 1
+
+#stripped down middleware
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
