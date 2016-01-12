ac930d6f2512f2b982bfd106776754f4157c9498
TB026891 <tim.baxter@cerner.com>
2016-01-12 16:58:57 -0600

    added middleware?

--- test_settings.py
+++ test_settings.py
@@ -32,4 +32,5 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.core.context_processors.request'
 )
